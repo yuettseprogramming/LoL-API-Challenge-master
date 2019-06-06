c91c847d8cf825f2312452e7bafccdc5656bdba9
Yuet Fu Tse <yuettse@hotmail.com>
2019-06-06 14:43:52 -0700

    update

---
+++ client/src/js/main.js
@@ -0,0 +1,139 @@
+/**
+ * Created by pl on 8/28/15.
+ */
+
+var getChampName = function(thisChampName){
+    var champarr = ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Ashe", "Azir", "Bard", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Cassiopeia", "Chogath", "Corki", "Darius", "Diana", "DrMundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "FiddleSticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Hecarim", "Heimerdinger", "Irelia", "Janna", "JarvanIV", "Jax", "Jayce", "Jinx", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kennen", "Khazix", "KogMaw", "Leblanc", "LeeSin", "Leona", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "MasterYi", "MissFortune", "Mordekaiser", "Morgana", "Nami", "Nasus", "Nautilus", "Nidalee", "Nocturne", "Nunu", "Olaf", "Orianna", "Pantheon", "Poppy", "Quinn", "Rammus", "RekSai", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Sejuani", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Syndra", "TahmKench", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "TwistedFate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Velkoz", "Vi", "Viktor", "Vladimir", "Volibear", "Warwick", "MonkeyKing", "Xerath", "XinZhao", "Yasuo", "Yorick", "Zac", "Zed", "Ziggs", "Zilean", "Zyra"];
+    var chamName = ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Ashe", "Azir", "Bard", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Cassiopeia", "Cho'Gath", "Corki", "Darius", "Diana", "Dr. Mundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Hecarim", "Heimerdinger", "Irelia", "Janna", "Jarvan IV", "Jax", "Jayce", "Jinx", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kennen", "Kha'Zix", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Mao'kai", "Master Yi", "Miss Fortune", "Mordekaiser", "Morgana", "Nami", "Nasus", "Nautilus", "Nidalee", "Nocturne", "Nunu", "Olaf", "Orianna", "Pantheon", "Poppy", "Quinn", "Rammus", "Rek'Sai", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Sejuani", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Syndra", "Tahm Kench", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vi", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xerath", "Xin Zhao", "Yasuo", "Yorick", "Zac", "Zed", "Ziggs", "Zilean", "Zyra"];
+    return  champarr[chamName.indexOf(thisChampName)];
+};
+
+var createGraph = function(barData){
+    var vis = d3.select('#visualisation'),
+        WIDTH = 900,
+        HEIGHT = 500,
+        MARGINS = {
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 50
+        },
+        xRange = d3.scale.ordinal().rangeRoundBands([MARGINS.left, WIDTH - MARGINS.right], 0.1).domain(barData.map(function (d) {
+            return d.name;
+        })),
+
+
+        yRange = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([0,
+            d3.max(barData, function (d) {
+                return d.values;
+            })
+        ]),
+
+        xAxis = d3.svg.axis()
+            .scale(xRange)
+            .tickSize(3)
+            .tickSubdivide(true),
+
+        yAxis = d3.svg.axis()
+            .scale(yRange)
+            .tickSize(3)
+            .orient("left")
+            .tickSubdivide(true);
+
+    vis.append('svg:g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0,' + (HEIGHT - MARGINS.bottom) + ')')
+        .style('fill', 'white')
+        .call(xAxis);
+
+    vis.append('svg:g')
+        .attr('class', 'y axis')
+        .attr('transform', 'translate(' + (MARGINS.left) + ',0)')
+        .style('fill', 'white')
+        .call(yAxis);
+
+    vis.selectAll('rect')
+        .data(barData)
+        .enter()
+        .append('rect')
+        .attr('x', function (d) {
+            return xRange(d.name);
+        })
+        .attr('y', function (d) {
+            return yRange(d.values);
+        })
+        .attr('width', xRange.rangeBand())
+        .attr('height', function (d) {
+            return ((HEIGHT - MARGINS.bottom) - yRange(d.values));
+        })
+        .attr('fill', '#BFA700')
+        .on("mouseover", function(d){return tooltip.style("visibility", "visible")
+            .html('<h4>'+ d.values+'</h4><img class="img-circle" width="60px" src="http://ddragon.leagueoflegends.com/cdn/5.15.1/img/champion/'+getChampName(d.name)+'.png"/>');})
+        .on("mousemove", function(){return tooltip.style("top",
+            (d3.event.pageY-110)+"px").style("left",(d3.event.pageX-25)+"px");})
+        .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+};
+
+var tooltip = d3.select("body")
+    .append("div")
+    .style("position", "absolute")
+    .style("z-index", "10")
+    .style("visibility", "hidden");
+
+var resetGraph = function(){
+    $('#visualisation').html('');
+};
+
+var getGraphData = function(highlow, criteria){
+    var barData = [];
+    $.get('./asset/json/highestlowest/highestlowest.json', function(data){
+        var sijd = data.chart[criteria];
+        var start, end;
+        if(highlow == 'high'){
+            start = 0;
+            end = 10;
+        } else if (highlow=='low'){
+            start = 10;
+            end = 0;
+        }
+        for (var i = start; i< Object.keys(sijd).length-end; i++){
+            barData.push(data.chart[criteria][Object.keys(sijd)[i]]);
+        }
+
+    }).done(function(){
+        createGraph(barData);
+    });
+};
+
+$(function(){
+    var currentSelection = 'Ban_Percentage';
+    $('#graphTitle').text('Ban Percentage');
+    var rand = Math.floor(Math.random() * (4 - 1) + 1);
+    $('body').css('background-image', 'url(./asset/background/'+rand+'.jpg)');
+    $.get('./asset/json/highestlowest/highestlowest.json', function(data){
+        for (var i = 0; i< Object.keys(data.chart).length; i++){
+            $('#links').append('<li><a href="#" id="'+Object.keys(data.chart)[i]+'" class="graphLinks">'+Object.keys(data.chart)[i].split('_').join(' ')+'</a></li>');
+        }
+        $('.graphLinks').click(function(e){
+            $('#graphTitle').text($(this).text());
+            currentSelection = $(this).attr('id');
+            resetGraph();
+            getGraphData('high', $(this).attr('id'));
+            e.preventDefault();
+        });
+    });
+    $('#high').click(function(e){
+        resetGraph();
+        getGraphData('high', currentSelection);
+        e.preventDefault();
+    });
+    $('#low').click(function(e){
+        resetGraph();
+        getGraphData('low', currentSelection);
+        e.preventDefault();
+    });
+    getGraphData('high', currentSelection);
+
+    /*$('#teamTable').append('<table class="table">' +
+        '<tr><td>diu lay</td></tr>');*/
+});
